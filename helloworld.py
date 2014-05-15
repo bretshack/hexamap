6096170ccadb2ccbe364d441471ca866b98a9a56
bretshack <bshack@gmail.com>
2014-05-15 20:35:04 +0000

    Added user service

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-"""A simple webapp2 server."""
+from google.appengine.api import users
 
 import webapp2
 
@@ -6,8 +6,13 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        user = users.get_current_user()
+
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
 
 
 application = webapp2.WSGIApplication([
