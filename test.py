286fa8fbc74ba9909a355a4d9c02489241ec2d24
bretshack <bshack@gmail.com>
2014-05-16 21:22:59 +0000

    Fixing missing main page

--- test.py
+++ test.py
@@ -13,6 +13,20 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render(url=url,
+                                                url_linktext=url_linktext))
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
