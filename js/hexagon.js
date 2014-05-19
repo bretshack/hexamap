b35a3665e54d695722c8157293268295035ff469
bretshack <bshack@gmail.com>
2014-05-19 20:16:21 +0000

    Trying a different drawing method

--- js/hexagon.js
+++ js/hexagon.js
@@ -5,19 +5,15 @@
 * Time: 04:38 PM
 */
 
-$('#svg_div').svg();
-var svg = $('#svg_div').svg("get");
-// call using polygon(x,y,rad,sides) where x and y are centre co-ordinates
-// rad is the radius of the vertices, and sides is the number of sides
-svg.path(polygon(15,15,60,6),{stroke: "black", "stroke-width":5, fill: "green",});
+
+var draw = SVG('drawing')
+var hex = draw.polygon(polygon(15,15,60,6)).fill('none').stroke({ width: 1 });
 
 
 function polygon(x,y,rad,sides) {
-	var path = "M ";
+	var path = "";
 	for (a=0;a<sides;a++) {
-		if (a>0) {path = path + "L ";}
-		path = path + (x+(Math.sin(2*Math.PI*a/sides)*rad)) + " " + (y-(Math.cos(2*Math.PI*a/sides)*rad)) + " ";
+		path = path + (x+(Math.sin(2*Math.PI*a/sides)*rad)) + "," + (y-(Math.cos(2*Math.PI*a/sides)*rad)) + " ";
 	}
-	path = path + "z";
-	return path;
+	return path.trimRight();
 }
