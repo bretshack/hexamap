71fa8262e235df0ed8362d2be41b3f0dca6798a5
bretshack <bshack@gmail.com>
2014-05-23 20:01:05 +0000

    Changes

--- js/app/grid.js
+++ js/app/grid.js
@@ -7,33 +7,44 @@
 define(["app/getScreenCenter", "d3", "app/hexagon"], function(getScreenCenter, d3, Hexagon) {
   "use strict";
   var _s32 = (Math.sqrt(3) / 2);
-  var hexsize = 30;
+  var rad = 30;
   var xCenter = getScreenCenter.x;
   var yCenter = getScreenCenter.y;
-  var shifty = hexsize * 3 / 2;
-  var shiftx = _s32 * hexsize;
+  var shifty = rad * 3 / 2;
+  var shiftx = _s32 * rad;
   
-  var test = "The start is ";
-  test = test + Math.sin(0)*30;
-  test = test + ", ";
-  test = test + Math.cos(0)*30;
+  var points = [{"x":rad, "y":0}, 
+                {'x':rad/2, 'y':rad*_s32}, 
+                {'x':-rad/2, 'y':rad*_s32}, 
+                {'x':-rad, 'y':0}, 
+                {'x':-rad/2, 'y':-rad*_s32}, 
+                {'x':rad/2, 'y':-rad*_s32}];
   
-  console.log(test);
+  var canvas = d3.select("#canvas").append("svg")
+                 .data([points]).enter()
+                 .append('polygon')
+                 .attr("points", function(d) { return [scaleX(d.x),scaleY(d.y)].join(",")})
+                 .attr("stroke", "black")
+                 .attr("stroke-width", 2);
   
   /*
-   * Hexagon shaped grid of radius 'gridsize' (i.e. 'gridsize' hexagons from center to vertex, not counting center hex)
-   *    * 
+   * Hexagon shaped grid of radius 'gridrad' (i.e. 'gridrad' hexagons from center to vertex, not counting center hex)
+   * 
    * Generic Hexagon Vertices:
-   * [size, 0], [size/2, size*_s32], [-size/2, size*_s32]
-   * [-size, 0], [-size/2, -size*_s32, [size/2, -size*_s32]
+   * [rad, 0], 
+   * [rad/2, rad*_s32], 
+   * [-rad/2, rad*_s32], 
+   * [-rad, 0], 
+   * [-rad/2, -rad*_s32], 
+   * [rad/2, -rad*_s32]
    * 
    * Generic Hexagon Path:
-   * M (Math.sin(0)*size) (Math.cos(0)*size)
-   * L (Math.sin(Math.PI/3)*size) (Math.cos(Math.PI/3)*size)
-   * L (Math.sin(Math.PI*2/3)*size) (Math.cos(Math.PI*2/3)*size)
-   * L (Math.sin(Math.PI*3/3)*size) (Math.cos(Math.PI*3/3)*size)
-   * L (Math.sin(Math.PI*4/3)*size) (Math.cos(Math.PI*4/3)*size)
-   * L (Math.sin(Math.PI*5/3)*size) (Math.cos(Math.PI*5/3)*size)
+   * M (Math.sin(0)*rad)           (Math.cos(0)*rad)
+   * L (Math.sin(Math.PI/3)*rad)   (Math.cos(Math.PI/3)*rad)
+   * L (Math.sin(Math.PI*2/3)*rad) (Math.cos(Math.PI*2/3)*rad)
+   * L (Math.sin(Math.PI*3/3)*rad) (Math.cos(Math.PI*3/3)*rad)
+   * L (Math.sin(Math.PI*4/3)*rad) (Math.cos(Math.PI*4/3)*rad)
+   * L (Math.sin(Math.PI*5/3)*rad)k (Math.cos(Math.PI*5/3)*rad)
    * z
    *  
   */
