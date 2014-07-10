2ac6353ab8f2b9b8f172dc663f1c2347c2994925
Bret Shack <bshack@gmail.com>
2014-07-10 17:26:02 -0400

    Trying to use D3js

--- js/hex.js
+++ js/hex.js
@@ -18,3 +18,31 @@ HxOverrides.iter = function(a) {
             return this.arr[this.cur++];
         }};
 }
+
+// (x, y) should be the center
+// scale should be the distance from corner to corner
+// orientation should be 0 (flat bottom hex) or 1 (flat side hex)
+function hexToPolygon(scale, x, y, orientation) {
+    // NOTE: the article says to use angles 0..300 or 30..330 (e.g. I
+    // add 30 degrees for pointy top) but I instead use -30..270
+    // (e.g. I subtract 30 degrees for pointy top) because it better
+    // matches the animations I needed for my diagrams. They're
+    // equivalent.
+    var points = [];
+    for (var i = 0; i < 6; i++) {
+        var angle = 2 * Math.PI * (2 * i - orientation) / 12;
+        points.push(new ScreenCoordinate(x + 0.5 * scale * Math.cos(angle),
+        y + 0.5 * scale * Math.sin(angle)));
+    }
+    return points;
+}
+
+// The shape of a hexagon is fixed by the scale and orientation
+function makeHexagonShape(scale, orientation) {
+    var points = hexToPolygon(scale, 0, 0, orientation);
+    var svg_coord = "";
+    points.forEach(function(p) {
+        svg_coord += p + " ";
+    });
+    return svg_coord;
+}
---
+++ js/lambda.js
@@ -0,0 +1,59 @@
+//This code taken from "Hexagon Grid Functions" on http://www.redblobgames.com/grids/hexagons.html (Copyright 2013 Red Blob Games <redblobgames@gmail.com>)
+
+var Lambda = function() { }
+Lambda.array = function(it) {
+	var a = new Array();
+	var $it0 = $iterator(it)();
+	while( $it0.hasNext() ) {
+		var i = $it0.next();
+		a.push(i);
+	}
+	return a;
+}
+Lambda.map = function(it,f) {
+	var l = new List();
+	var $it0 = $iterator(it)();
+	while( $it0.hasNext() ) {
+		var x = $it0.next();
+		l.add(f(x));
+	}
+	return l;
+}
+var List = function() {
+	this.length = 0;
+};
+List.prototype = {
+	add: function(item) {
+		var x = [item];
+		if(this.h == null) this.h = x; else this.q[1] = x;
+		this.q = x;
+		this.length++;
+	}
+	,iterator: function() {
+		return { h : this.h, hasNext : function() {
+			return this.h != null;
+		}, next : function() {
+			if(this.h == null) return null;
+			var x = this.h[0];
+			this.h = this.h[1];
+			return x;
+		}};
+	}
+}
+
+var Std = function() { }
+function $iterator(o) { if( o instanceof Array ) return function() { return HxOverrides.iter(o); }; return typeof(o.iterator) == 'function' ? $bind(o,o.iterator) : o.iterator; };
+var $_;
+function $bind(o,m) { var f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; return f; };
+Math.__name__ = ["Math"];
+Math.NaN = Number.NaN;
+Math.NEGATIVE_INFINITY = Number.NEGATIVE_INFINITY;
+Math.POSITIVE_INFINITY = Number.POSITIVE_INFINITY;
+Math.isFinite = function(i) {
+	return isFinite(i);
+};
+Math.isNaN = function(i) {
+	return isNaN(i);
+};
+Cube._directions = [[1,-1,0],[1,0,-1],[0,1,-1],[-1,1,0],[-1,0,1],[0,-1,1]];
+Grid.SQRT_3_2 = Math.sqrt(3) / 2;
